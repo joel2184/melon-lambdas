cc37f60881fff5777a07a7607ce2855b33f2da60
Joel Hurtado <joel29102000@gmail.com>
2022-05-18 09:55:10 +0200

    CLASE MELON

---
+++ src/main/java/com/sinensia/lambdas/Melon.java
@@ -0,0 +1,50 @@
+package com.sinensia.lambdas;
+
+public class Melon {
+
+    private String type;
+    private int weight;
+    private String origin;
+
+    public Melon()
+    {
+    }
+    public Melon(String type, int weight, String origin) {
+        this.type = type;
+        this.weight = weight;
+        this.origin = origin;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    @Override
+    public String toString() {
+        return "Melon{" +
+                "type='" + type + '\'' +
+                ", weight=" + weight +
+                ", origin='" + origin + '\'' +
+                '}';
+    }
+}
